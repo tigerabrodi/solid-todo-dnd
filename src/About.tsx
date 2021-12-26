2a73c4853b437bd7f07389026bf973a0dadfd247
tigerabrodi <tigerabrodi@gmail.com>
2021-12-26 14:50:27 +0100

    feat about page and app

---
+++ src/About.tsx
@@ -0,0 +1,9 @@
+import type { Component } from "solid-js";
+
+export const About: Component = () => {
+  return (
+    <h1 className="text-2xl mt-10">
+      Made by a stronk ninja named Tiger Abrodi
+    </h1>
+  );
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import type { Component } from "solid-js";
+import { Routes, Route, Link } from "solid-app-router";
+import { Home } from "./Home";
+import { About } from "./About";
 
 const App: Component = () => {
-  return <div className="text-white">Hello World</div>;
+  return (
+    <>
+      <nav className="flex flex-col items-center">
+        <Link href="/" className="bg-white text-blue mt-5 text-lg px-2">
+          Home
+        </Link>
+        <Link href="/about" className="bg-white text-blue mt-2 text-lg px-2">
+          About
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
